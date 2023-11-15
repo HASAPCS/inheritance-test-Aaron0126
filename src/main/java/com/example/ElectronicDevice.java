696d2c4be7c4cfaa1878909b6fb1ab4987ab9955
Aaron0126 <aaron.lin@has.hc.edu.tw>
2023-11-15 10:17:44 +0800

    done

--- src/main/java/com/example/ElectronicDevice.java
+++ src/main/java/com/example/ElectronicDevice.java
@@ -2,10 +2,19 @@ package com.example;
 
 public class ElectronicDevice {
     // TODO: Declare common attributes for electronic devices, such as brand and model.
-
+    private String brand;
+    private String model;
     // Constructor to initialize an ElectronicDevice object
     public ElectronicDevice(String brand, String model) {
         // TODO: Initialize the common attributes (brand, model) with the provided parameters
+        this.brand=brand;
+        this.model=model;
+    }
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
     }
 
     // TODO: Implement getters (and setters if necessary) for the brand and model.
--- src/main/java/com/example/Smartphone.java
+++ src/main/java/com/example/Smartphone.java
@@ -2,14 +2,22 @@ package com.example;
 
 // TODO: make this class extend ElectronicDevice
 
-public class Smartphone {
+public class Smartphone extends ElectronicDevice {
+    private String brand;
+    private String model;
+    private int batteryLevel;
     // TODO: Declare the necessary attributes (brand, model, batteryLevel)
 
     // Constructor to initialize the Smartphone object
     public Smartphone(String brand, String model, int batteryLevel) {
+        super(brand, model);
+        this.batteryLevel=batteryLevel;
         // TODO: Initialize the attributes with the provided parameters
     }
-
+    public int getBatteryLevel(){
+        return this.batteryLevel;
+    }
+   
     // TODO: Implement a method to check the battery level and return a message based on the level
     // Implement the checkBattery method to return:
         // "Warning: Battery low!" if the battery level is 20% or lower.
